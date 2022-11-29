2b045858de16039500e4dedf22d3ff9152b0cfc6
JadhavDnyaneshwarr <118913484+JadhavDnyaneshwarr@users.noreply.github.com>
2022-11-29 22:51:30 +0530

    The employee monthly salary using function and while loop.

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,29 +4,43 @@
 
 attendence=$(( RANDOM%3 ));
 wage_per_hr=20;
-days_of_month=20;
+days_of_month=1;
 daily_working_hr=8;
+totalWoorkingHour=0;
 
-case $attendence in
-	1)
-		echo "Employee is present for full day work";
-		working_hr=$daily_working_hr
-	;;
+function Employee_salary(){
+	while [[ $days_of_month -le 20 && $totalWoorkingHour -lt 100 ]]
+	do
+
+		case $attendence in
+			1)
+				echo "Employee is present for full day work";
+				working_hr=$daily_working_hr
+			;;
 	
-	2)
-		echo "Employee is present for part time work";
-		working_hr=$(($daily_working_hr/2))
-	;;
+			2)
+				echo "Employee is present for part time work";
+				working_hr=$(($daily_working_hr/2))
+			;;
 	
-	*)
-		echo "Employee is absent";
-		working_hr=0
-	;;
-esac	
+			*)
+				echo "Employee is absent";
+				working_hr=0
+			;;
+		esac	
 
-
-daily_wage_employee=$(( $wage_per_hr * $working_hr));
-echo "Employee daily wage is " $daily_wage_employee;
-monthly_salary=$(( $daily_wage_employee * $days_of_month ));
-echo "The employee's month salary is $monthly_salary";
+		totalWoorkingHour=$(( $totalWoorkingHour + $working_hr ));
+		if [ $totalWoorkingHour -gt 100 ]
+		then
+			totalWorkingHour=$(( $working_hr - $totalWoorkingHour ));
+			break;
+		fi
+		salary=$(( $working_hr - $totalWoorkingHour ));
+		totalsalary=$((totalsalary + $salary));
+		
+		((days_of_month++));
+	done
+	echo "Employee has earned $totalsalary in a month (Total working Hour : $totalWorkingHour)";
+}
+Employee_salary
 
