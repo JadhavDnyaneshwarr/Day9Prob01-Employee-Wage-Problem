8d36d70e445f6db3580eaa31bf17b7a7daf1d7b2
JadhavDnyaneshwarr <118913484+JadhavDnyaneshwarr@users.noreply.github.com>
2022-11-29 20:43:18 +0530

    Emloyee daily wage based on full time and pat time work.

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,6 +5,7 @@
 attendence=$(( RANDOM%3 ));
 wage_per_hr=20
 full_day_hr=8
+part_time_hr=4
 
 if [ $attendence -eq 1 ]
 then
@@ -15,6 +16,9 @@ then
 elif [ $attendence -eq 2 ]
 then
 	echo "Employee is present for part time work";
+	daily_wage_employee=$(( $wage_per_hr * $part_time_hr));
+	echo $daily_wage_employee;
+
 else
 	echo "Employee is absent";
 fi
