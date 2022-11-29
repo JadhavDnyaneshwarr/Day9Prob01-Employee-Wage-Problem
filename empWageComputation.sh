ffe6908ddf41c47cea9c5c3381de606418182f6c
JadhavDnyaneshwarr <118913484+JadhavDnyaneshwarr@users.noreply.github.com>
2022-11-29 21:03:13 +0530

    Daily Employee wage by using Case statement

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -6,20 +6,26 @@ attendence=$(( RANDOM%3 ));
 wage_per_hr=20
 full_day_hr=8
 part_time_hr=4
+daily_working_hr=8
 
-if [ $attendence -eq 1 ]
-then
-	echo "Employee is present for full day work";
-	daily_wage_employee=$(( $wage_per_hr * $full_day_hr));
-	echo $daily_wage_employee;
+case $attendence in
+	1)
+		echo "Employee is present for full day work";
+		working_hr=$daily_working_hr
+	;;
+	
+	2)
+		echo "Employee is present for part time work";
+		working_hr=$(($daily_working_hr/2))
+	;;
+	
+	*)
+		echo "Employee is absent";
+		working_hr=0
+	;;
+esac	
 
-elif [ $attendence -eq 2 ]
-then
-	echo "Employee is present for part time work";
-	daily_wage_employee=$(( $wage_per_hr * $part_time_hr));
-	echo $daily_wage_employee;
 
-else
-	echo "Employee is absent";
-fi
+daily_wage_employee=$(( $wage_per_hr * $working_hr));
+echo "Employee daily wage is " $daily_wage_employee;
 
